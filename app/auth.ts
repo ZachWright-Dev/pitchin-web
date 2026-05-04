27b6d9412966fc1e74a1b100a46435d8e05b15ae
Zach Wright <zwright5270@gmail.com>
2026-05-04 01:03:36 -0400

    adding userID to session

--- app/auth.ts
+++ app/auth.ts
@@ -9,5 +9,15 @@ const adapter = PrismaAdapter(prismaForAuth)
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: adapter,
   providers: [Google],
-  session: { strategy: "jwt" }, 
+  session: { strategy: "jwt" },
+  callbacks: {
+    jwt({ token, user}) {
+      if (user) token.id = user.id
+      return token;
+    },
+    session({ session, token}) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
 })
